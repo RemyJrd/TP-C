849394388bc915d2fd637731e15efe5a540a8ecd
remy <remyjardinpro@gmail.com>
2022-05-22 02:54:09 +0200

    first step

---
+++ TP 3/functions.h
@@ -0,0 +1,9 @@
+#ifndef functions.c
+#define functions.c
+
+int f1() {
+
+    return 0;
+}
+
+#endif
---
+++ TP 3/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <functions.h>
+#include <functions.c>
+
+int main() {
+
+	return 0;
+}

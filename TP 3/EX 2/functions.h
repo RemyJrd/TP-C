6032d8a6ddda7383f61b81b204af81375b1e5d11
remy <remyjardinpro@gmail.com>
2022-05-29 02:51:34 +0200

    EX 2 done and EX 1 Update (time srand fix)

--- TP 3/EX 2/functions.h
+++ TP 3/EX 2/functions.h
@@ -3,6 +3,19 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
+typedef struct {
+    int L;
+    int C;
+    int* TabL;
+    int* TabC;
+} GRILLE;
+
+GRILLE* newGrille(int L, int C);
+GRILLE initGrille();
+GRILLE afficheGrille(GRILLE* g);
+int randomizer(void);
 
 
 #endif
--- TP 3/EX 2/main.c
+++ TP 3/EX 2/main.c
@@ -1,7 +1,23 @@
 #include "functions.h"
 #include "functions.c"
+#include <time.h>
 
 int main() {
+    int L, C;
+    GRILLE* g;
+    srand(time(0));
+
+    printf("Nombres de Lignes : ");
+    scanf("%d", &L);
+    printf("Nombres de Colonnes : ");
+    scanf("%d", &C);
+
+    g = newGrille(L, C);
+
+    initGrille(g);
+
+    printf("--------- Resultats Obtenus -----------\n");
+    afficheGrille(g);
 
     return 0;
 }
